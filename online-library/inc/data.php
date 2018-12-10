6261f358db2af8c12120f761874bb4c85f461723
sunyoutian <45578590+sunyoutian@users.noreply.github.com>
2018-12-10 18:10:34 -0500

    Add files via upload

---
+++ online-library/inc/data.php
@@ -0,0 +1,186 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Page One",
+	"img" => "img/media/6",
+    "genre" => "Documentary",
+    "format" => "Paperback",
+    "year" => 1994,
+    "category" => "Books",
+    "authors" => [
+        "Erich Gamma",
+        "Richard Helm",
+        "Ralph Johnson",
+        "John Vlissides"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0201633610'
+];
+$catalog[102] = [
+    "title" => "we student",
+    "img" => "img/media/5.jpg",
+    "genre" => "life",
+    "format" => "Ebook",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Robert C. Martin"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '978-0132350884'
+];
+$catalog[103] = [
+    "title" => "The Euro",
+    "img" => "img/media/4.jpg",
+    "genre" => "Economic",
+    "format" => "Hardcover",
+    "year" => 1999,
+    "category" => "Books",
+    "authors" => [
+        "Martin Fowler",
+        "Kent Beck",
+        "John Brant",
+        "William Opdyke",
+        "Don Roberts"
+    ],
+    "publisher" => "Addison-Wesley Professional",
+    "isbn" => '978-0201485677'
+];
+$catalog[104] = [
+    "title" => "Currency War",
+    "img" => "img/media/7.jpg",
+    "genre" => "Economic",
+    "format" => "Audio",
+    "year" => 2011,
+    "category" => "Books",
+    "authors" => [
+        "Robert C. Martin"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '007-6092046981'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Avengers",
+    "img" => "img/media/11.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 2012,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "Winston Groom",
+        "Eric Roth"
+    ],
+    "stars" => [
+        "Cris Evens",
+        "Rebecca Williams",
+        "Sally Field",
+        "Michael Conner Humphreys"
+    ]
+];
+$catalog[202] = [
+    "title" => "star wars",
+    "img" => "img/media/10.jpg",
+    "genre" => "Comedy",
+    "format" => "Blu-ray",
+    "year" => 1999,
+    "category" => "Movies",
+    "director" => "Mike Judge",
+    "writers" => [
+        "William Goldman"
+    ],
+    "stars" => [
+        "Ron Livingston",
+        "Jennifer Aniston",
+        "David Herman",
+        "Ajay Naidu",
+        "Diedrich Bader",
+        "Stephen Root"
+    ]
+];
+$catalog[203] = [
+    "title" => "Detective Conan",
+    "img" => "img/media/9.jpg",
+    "genre" => "Drama",
+    "format" => "Blu-ray",
+    "year" => 1996,
+    "category" => "Movies",
+    "director" => "Peter Jackson",
+    "writers" => [
+        "J.R.R. Tolkien",
+        "Fran Walsh",
+        "Philippa Boyens",
+        "Peter Jackson"
+    ],
+    "stars" => [
+        "Ron Livingston",
+        "Jennifer Aniston",
+        "David Herman",
+        "Ajay Naidu",
+        "Diedrich Bader",
+        "Stephen Root"
+    ]
+];
+$catalog[204] = [
+    "title" => "Harry Poter",
+    "img" => "img/media/3.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2001,
+    "category" => "Movies",
+    "director" => "Rob Reiner",
+    "writers" => [
+        "William Goldman"
+    ],
+    "stars" => [
+        "Cary Elwes",
+        "Mandy Patinkin",
+        "Robin Wright",
+        "Chris Sarandon",
+        "Christopher Guest",
+        "Wallace Shawn",
+        "André the Giant",
+        "Fred Savage",
+        "Peter Falk",
+        "Billy Crystal"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "my uncle hulot",
+    "img" => "img/media/8.jpg",
+    "genre" => "Clasical",
+    "format" => "CD",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "charles benin"
+];
+$catalog[302] = [
+    "title" => "shin chan",
+    "img" => "img/media/1.png",
+    "genre" => "Rock",
+    "format" => "Vinyl",
+    "year" => 2015,
+    "category" => "Music",
+    "artist" => "shin chan"
+];
+$catalog[303] = [
+    "title" => "snopy",
+    "img" => "img/media/2.jpg",
+    "genre" => "Country",
+    "format" => "Cassette",
+    "year" => 1990,
+    "category" => "Music",
+    "artist" => "Garth Brooks"
+];
+$catalog[304] = [
+    "title" => "little bue",
+    "img" => "img/media/12.jpg",
+    "genre" => "Jaz",
+    "format" => "MP3",
+    "year" => 2008,
+    "category" => "Music",
+    "artist" => "Nat King Cole"
+];
---
+++ online-library/products.php
@@ -0,0 +1,162 @@
+<?php //Books*
+$catalog[101] = [
+   "title" => "It: A Novel (reissue)",
+   "img" => "img/media/bookofjoy.jpg",
+  "genre" => "Horror",
+  "format" => "Paperback",
+  "year" => 2016,
+  "category" => "Books",
+  "authors" => "Stephen King",
+  "publisher" => "Scribner",
+  "isbn" => '9781501142970'
+];
+
+$catalog[102] = [
+   "title" => "Pet Sematary",
+   "img" => "img/media/bookofjoy.jpg",
+  "genre" => "Horror",
+  "format" => "Hardcover",
+  "year" => 2002,
+  "category" => "Books",
+  "authors" => "Stephen King",
+  "publisher" => "Gallery Books",
+  "isbn" => '9780743412285'
+];
+
+
+$catalog[103] = [
+   "title" => "The Mist",
+   "img" => "img/media/bookofjoy.jpg",
+  "genre" => "Horror",
+  "format" => "Paperback",
+  "year" => 2018,
+  "category" => "Books",
+  "authors" => "Stephen King",
+  "publisher" => "Scribner",
+  "isbn" => '	9781982103521'
+];
+
+
+$catalog[101] = [
+   "title" => "Cujo: A Novel",
+   "img" => "img/media/bookofjoy.jpg",
+  "genre" => "Horror",
+  "format" => "Hardover",
+  "year" => 2016,
+  "category" => "Books",
+  "authors" => "Stephen King"
+  "publisher" => "Gallery Books",
+  "isbn" => '9781501192241'
+];
+
+//MOVIES
+$catalog[201] = [
+  "title" => "The Godfather Collection",
+  "img" => "img/media/avatar.jpg",
+  "genre" => "Drama",
+  "format" => "DVD",
+  "year" => 2008,
+  "category" => "Movies",
+  "director" => "Francis Ford Coppola",
+  "writers" => [
+      "Francis Ford Coppolan",
+   "Mario Puzo"
+  ],
+  "stars" => [
+      "Al Pacino",
+      "Robert Duvall",
+      "Diane Keaton"
+  ]
+];
+
+$catalog[202] = [
+  "title" => "Rocky: Heavyweight Collection",
+  "img" => "img/media/avatar.jpg",
+  "genre" => "Action,Drama",
+  "format" => "DVD",
+  "year" => 2014,
+  "category" => "Movies",
+  "director" => "John G. Avildsen",
+  "writers" => "Sylvester Stallone"
+  "stars" => "Sylvester Stallone "
+];
+
+$catalog[203] = [
+  "title" => "Die Hard Collection",
+  "img" => "img/media/avatar.jpg",
+  "genre" => "Action",
+  "format" => "DVD",
+  "year" => 2018,
+  "category" => "Movies",
+  "director" => "Renny Harlin",
+  "writers" => [
+      "Doug Richardson",
+      "Steven E. de Souza",
+  ],
+  "stars" => [
+      "Bruce Willis",
+      "Bonnie Bedelia ",
+  ]
+];
+
+$catalog[204] = [
+  "title" => "The Expendables: 3-Film Collection",
+  "img" => "img/media/avatar.jpg",
+  "genre" => "Action,",
+  "format" => "DVD",
+  "year" => 2017,
+  "category" => "Movies",
+  "director" => "Sylvester Stallone",
+  "writers" => [
+      "Sylvester Stallone",
+      "Dave Callaham",
+  ],
+  "stars" => [
+      "Sylvester Stallone",
+      "Jason Statham",
+      "Mickey Rourke"
+  ]
+];
+
+//MUSIC//
+$catalog[301] = [
+  "title" => "Road to Ruin (reissue)",
+  "img" => "img/media/beethoven.jpg",
+  "genre" => "Rock",
+  "format" => "Vinyl",
+  "year" => 2018,
+  "category" => "Music",
+  "artist" => "Ramones"
+];
+
+$catalog[302] = [
+  "title" => "Greatest Hits",
+  "img" => "img/media/beethoven.jpg",
+  "genre" => "Rock",
+  "format" => "CD, Vinyl",
+  "year" => 2004,
+  "category" => "Music",
+  "artist" => "Queen"
+];
+
+$catalog[303] = [
+  "title" => "Thriller (reissue)",
+  "img" => "img/media/beethoven.jpg",
+  "genre" => "Pop",
+  "format" => "Vinyl",
+  "year" => 2015,
+  "category" => "Music",
+  "artist" => "Micheal Jackson"
+];
+
+$catalog[304] = [
+  "title" => "Abbey Road (reissue)",
+  "img" => "img/media/beethoven.jpg",
+  "genre" => "Rock, Pop",
+  "format" => "Vinyl",
+  "year" => 2012,
+  "category" => "Music",
+  "artist" => "The beatles"
+];
+
+?>
